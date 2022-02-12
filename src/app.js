917f1e138047653544d6b306f44837636613bf77
Norberto Mangiacone <mangiaconenorberto@gmail.com>
2022-02-12 12:21:52 -0300

    deploy

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const { CORS_URL } = process.env
+const { CORS_URL } = process.env;
+const cors = require("cors");
+
+
 
 require('./db.js');
 
@@ -11,12 +14,14 @@ const server = express();
 
 server.name = 'API';
 
+server.use(cors());
+
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://front-asalallena.vercel.app/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
